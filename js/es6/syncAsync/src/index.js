42f4344f367a76e4099e50cffb24fec8c0d5d865
JoaoVasconcelosV <65971039+JoaoVasconcelosV@users.noreply.github.com>
2021-09-23 15:12:00 -0300

    Att das aulas

---
+++ js/es6/syncAsync/src/index.js
@@ -0,0 +1,11 @@
+function step02() {
+  console.log("Passo 02");
+}
+
+console.log("Passo 01");
+step02();
+console.log("Passo 03");
+setTimeout(() => {
+  console.log("Passo 04");
+}, 2000)
+console.log("Passo 05");
